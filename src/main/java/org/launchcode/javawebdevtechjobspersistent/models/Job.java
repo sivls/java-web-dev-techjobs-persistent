1fb78d32d6eabb1ff78718b8ec43d0b79f212aa6
siv <shelby.schneider24@gmail.com>
2021-07-15 15:09:54 -0500

    wip again

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
@@ -1,3 +1,44 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
-//todo: code
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Job extends AbstractEntity {
+    @ManyToOne
+    private Employer employer;
+
+    @NotNull
+    @ManyToMany
+    private List<Skill> skills = new ArrayList<>();
+
+    public Job() {
+    }
+
+    public Job(Employer anEmployer, List<Skill> someSkills) {
+        super();
+        this.employer = anEmployer;
+        this.skills = someSkills;
+    }
+
+    // Getters and setters.
+
+    public Employer getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(Employer employer) {
+        this.employer = employer;
+    }
+
+    public List<Skill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<Skill> skills) {
+        this.skills = skills;
+    }
+}
